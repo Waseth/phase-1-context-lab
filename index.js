34bf1d32c0be714cfe3867a0afa5aae04389cac8
Waseth <waseth.emmanuel@student.moringaschool.com>
2024-04-15 09:41:48 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Function to create an employee record from an array
 let createEmployeeRecord = function(row){
     return {
         firstName: row[0],
@@ -10,85 +9,77 @@ let createEmployeeRecord = function(row){
     }
 }
 
-// Function to create employee records from an array of arrays
 let createEmployeeRecords = function(employeeRowData) {
     return employeeRowData.map(function(row){
         return createEmployeeRecord(row)
     })
 }
 
-// Function to add a time in event for an employee
-let createTimeInEvent = function(employeeRecord, dateStamp){
+let createTimeInEvent = function(dateStamp){
     let [date, hour] = dateStamp.split(' ')
 
-    employeeRecord.timeInEvents.push({
+    this.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(hour, 10),
         date,
     })
 
-    return employeeRecord
+    return this
 }
 
-// Function to add a time out event for an employee
-let createTimeOutEvent = function(employeeRecord, dateStamp){
+let createTimeOutEvent = function(dateStamp){
     let [date, hour] = dateStamp.split(' ')
 
-    employeeRecord.timeOutEvents.push({
+    this.timeOutEvents.push({
         type: "TimeOut",
         hour: parseInt(hour, 10),
         date,
     })
 
-    return employeeRecord
+    return this
 }
 
-// Function to calculate hours worked on a specific date
-let hoursWorkedOnDate = function(employeeRecord, soughtDate){
-    let inEvent = employeeRecord.timeInEvents.find(function(e){
+let hoursWorkedOnDate = function(soughtDate){
+    let inEvent = this.timeInEvents.find(function(e){
         return e.date === soughtDate
     })
 
-    let outEvent = employeeRecord.timeOutEvents.find(function(e){
+    let outEvent = this.timeOutEvents.find(function(e){
         return e.date === soughtDate
     })
 
     return (outEvent.hour - inEvent.hour) / 100
 }
 
-// Function to calculate wages earned on a specific date
-let wagesEarnedOnDate = function(employeeRecord, dateSought){
-    let rawWage = hoursWorkedOnDate(employeeRecord, dateSought) * employeeRecord.payPerHour
+let wagesEarnedOnDate = function(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
     return parseFloat(rawWage.toString())
 }
 
-// Function to calculate all wages for an employee
-let allWagesFor = function(employeeRecord){
-    let eligibleDates = employeeRecord.timeInEvents.map(function(e){
+let allWagesFor = function(){
+    let eligibleDates = this.timeInEvents.map(function(e){
         return e.date
     })
 
     let payable = eligibleDates.reduce(function(memo, d){
-        return memo + wagesEarnedOnDate(employeeRecord, d)
-    }, 0)
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
 
     return payable
 }
 
-// Function to find an employee by first name
 let findEmployeeByFirstName = function(srcArray, firstName) {
-    return srcArray.find(function(rec){
-        return rec.firstName === firstName
-    })
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
 }
 
-// Function to calculate total payroll for an array of employee records
 let calculatePayroll = function(arrayOfEmployeeRecords){
     return arrayOfEmployeeRecords.reduce(function(memo, rec){
-        return memo + allWagesFor(rec)
+        return memo + allWagesFor.call(rec)
     }, 0)
 }
-
 
 
 
